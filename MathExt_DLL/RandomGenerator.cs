9d5f0c8e1ccc6b09c6c99850fe7ca40d3d16e179
Elias Ruemmler <git@rc-art.de>
2018-03-12 09:17:06 +0100

    Clean code

--- MathExt_DLL/RandomGenerator.cs
+++ MathExt_DLL/RandomGenerator.cs
@@ -12,22 +12,21 @@ namespace Rca.MathExt
         /// <param name="sigma">Abweichung (2s, 3s...)</param>
         /// <param name="n">Anzahl der zu erzeugenden Werte</param>
         /// <returns>Zufallszahlen</returns>
-        static public List<double> NormalDistributed(double mean, double sigma, int n)
+        static public List<double> NormalDistributed(double mean, double sigma, int n = 100)
         {
             if (n <= 0)
-                throw new ArgumentException("Übergebenene Anzahl darf nicht kleiner-gleich 0 sein");
+                throw new ArgumentException("Anzahl darf nicht kleiner-gleich 0 sein, übergeben wurde n = " + n);
 
-            List<double> result = new List<double>();
-
-            Random randGenerator = new Random();
+            var result = new List<double>();
+            var rnd = new Random();
 
             for (int i = 0; i < n; i++)
             {
-                double u1 = randGenerator.NextDouble();
-                double u2 = randGenerator.NextDouble();
+                var u1 = rnd.NextDouble();
+                var u2 = rnd.NextDouble();
                 //double randStdNorm = Math.Sqrt(-2.0 * Math.Log(u1)) * Math.Sin(2.0 * Math.PI * u2);
-                double randStdNorm = Math.Sqrt(-2.0 * Math.Log(u1)) * Math.Cos(2.0 * Math.PI * u2);
-                result.Add(mean + sigma * randStdNorm);
+                double rndStdNorm = Math.Sqrt(-2.0 * Math.Log(u1)) * Math.Cos(2.0 * Math.PI * u2);
+                result.Add(mean + sigma * rndStdNorm);
             }
 
             return result;
--- MathExt_Testbench/Program.cs
+++ MathExt_Testbench/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Rca.MathExt
 {
@@ -10,17 +7,15 @@ namespace Rca.MathExt
     {
         static void Main(string[] args)
         {
-            List<double> myRandVals = RandomGenerator.NormalDistributed(0, 10, 1000);
+            var myRandVals = RandomGenerator.NormalDistributed(0, 10, 1000);
 
             myRandVals.Sort();
 
             foreach (double val in myRandVals)
-            {
                 Console.WriteLine(val.ToString());
-            }
 
+            Console.WriteLine();
             Console.WriteLine((myRandVals.Max() + myRandVals.Min()).ToString());
-
             Console.ReadKey();
         }
     }
